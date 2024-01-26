852d70c7b72e664434d8f1ba223b1a538918b614
John F. Davis <john.davis@broadcom.com>
2024-01-26 11:06:13 -0500

    wip

--- setenv.sh
+++ setenv.sh
@@ -9,9 +9,11 @@ export PATH=${HOME}/zopen/dev/depot_toolsport/depot_tools:$PATH
 # Disable updating depot_tools
 export DEPOT_TOOLS_UPDATE=0
 
-# Using depot_tools disable metrics.  
-# Comment out this line if you wish to participate
+# Disable commands:
+# * ensure_bootstrap
+# * update_depot_tools
+export DEPOT_TOOLS_BOOTSTRAP_PYTHON3=0
+
+# Using depot_tools disable metrics.
+# Comment out this line if you wish to participate.
 gclient metrics --opt-out
-
-
-
