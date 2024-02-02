dcad638b1fb118b053b662bdeb7bce1488239288
John F. Davis <john.davis@broadcom.com>
2024-02-02 14:32:31 -0500

    updated for v8base manual workflow

--- setenv.sh
+++ setenv.sh
@@ -20,5 +20,5 @@ gclient metrics --opt-out
 
 # remove any previous attempts of fetch
 # so as to pull a fresh copy
-rm ${HOME}/zopen/dev/.gclient_entries
-rm ${HOME}/zopen/dev/.gclient
+rm ${HOME}/zopen/dev/v8base/.gclient_entries
+rm ${HOME}/zopen/dev/v8base/.gclient
