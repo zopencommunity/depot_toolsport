e0b9f542ce71eb683409e0dc41795dd144cd83ae
John F. Davis <john.davis@broadcom.com>
2024-02-16 14:00:52 -0500

    wip

--- setenv.sh
+++ setenv.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
-# update the path to find depot_tools
-#export PATH=${PATH}:${HOME}/zopen/dev/depot_toolsport/depot_tools
-export PATH=${PATH}:${HOME}/zopen/usr/local/zopen/depot_tools/depot_tools-main
+# The path and environment variables are updated as a result of the package install.
+# However, if the user installs the package and does not run the zopen-config
+# command in the current shell, then the path will not be there yet.  In 
+# this case check for the path in the current path.
 
+desired_path="zopen/usr/local/zopen/depot_tools/depot_tools-man"
+
+if  expr "${PATH}" : ".*${desired_path}.*" 1>/dev/null ; then
+    echo "Good. depot_tools is in path."
+else
+    echo "Path to depot_tools missing.  Adding it to path.."
+    export PATH=${PATH}:${HOME}/${desired_path}
+fi
 
 
 # activate the virtual env
+echo "activating the virtual env - venv"
 . ${HOME}/zopen/usr/local/zopen/depot_tools/depot_tools-main//venv/bin/activate
+
+# update the virtual env
+python -m pip install -U pip
 
 
 # install the necessary requirements
-echo "    Installing packages via requirements.txt..."
+echo "Installing packages via requirements.txt..."
 pip install -r ${HOME}/zopen/usr/local/zopen/depot_tools/depot_tools-main/requirements.txt
 
 # Using depot_tools disable metrics.
@@ -22,42 +35,40 @@ gclient metrics --opt-out
 # provide a mechanism to resume work on v8 versus 
 # ensure pristine.   Assume by default the user
 # desires a pristine state and a fresh fetch.
-#RESUME=0
-#while [ $# -gt 0 ]; do
-#    case "$1" in
-#        -resume)
-#            echo "-resume switch used.  Skipping .gclient* delete"
-#            RESUME=1
-#            shift
-#            ;;
-#        -optionx)
-#            echo "option x used with value $2"
-#            echo "TODO: nothing to do yet"
-#            shift 2
-#            ;;
-#        *)
-#            echo "unknown option"
-#            echo "USAGE: $0 [-resume]"
-#            exit 1
-#            ;;
-#    esac
-#done
+RESUME=0
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -resume)
+            echo "-resume switch used.  Skipping .gclient* delete"
+            RESUME=1
+            shift
+            ;;
+        -optionx)
+            echo "option x used with value $2"
+            echo "TODO: nothing to do yet"
+            shift 2
+            ;;
+        *)
+            echo "unknown option"
+            echo "USAGE: $0 [-resume]"
+            exit 1
+            ;;
+    esac
+done
 
 # remove any previous attempts of fetch
 # so as to pull a fresh copy.  If this is the first time building
 # depot tools, these file will not be present and thus
 # no need to attempt to remove them. 
 # NOTE: assuming v8 will be built in $HOME/zopen/dev/v8base
-#if [ ${RESUME} -eq 0 ]; then
-#    echo "Attempting to delete any leftover .gclient* entries"
-#    if [ -d "${HOME}/zopen/dev/v8base" ]; then
-#        if [ -f "${HOME}/zopen/dev/v8base/.gclient_entries" ]; then
-#            rm ${HOME}/zopen/dev/v8base/.gclient_entries
-#        fi
-#        if [ -f "${HOME}/zopen/dev/v8base/.gclient_entries" ]; then
-#            rm ${HOME}/zopen/dev/v8base/.gclient
-#        fi
-#    fi
-#fi
-
-
+if [ ${RESUME} -eq 0 ]; then
+    echo "Attempting to delete any leftover .gclient* entries"
+    if [ -d "${HOME}/zopen/dev/v8base" ]; then
+        if [ -f "${HOME}/zopen/dev/v8base/.gclient_entries" ]; then
+            rm ${HOME}/zopen/dev/v8base/.gclient_entries
+        fi
+        if [ -f "${HOME}/zopen/dev/v8base/.gclient_entries" ]; then
+            rm ${HOME}/zopen/dev/v8base/.gclient
+        fi
+    fi
+fi
