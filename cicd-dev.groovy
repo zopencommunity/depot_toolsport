e55cd42ca17a222a12b1a5f316bd077b75bf5479
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:00 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/depot_toolsport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/depot_toolsport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/depot_toolsport.git'), string(name: 'PORT_DESCRIPTION', value: 'Tools for working with Chromium development. It requires python 3.8' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/depot_toolsport.git'), string(name: 'PORT_DESCRIPTION', value: 'Tools for working with Chromium development. It requires python 3.8' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
